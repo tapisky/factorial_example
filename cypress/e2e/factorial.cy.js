7d3f6c5086e7190f3dc8a2ce48812a0232a6cc7c
tapisky <jctapiador@gmail.com>
2022-10-02 02:20:10 +0200

    Create test for qainterview factorial web page

--- cypress/e2e/factorial.cy.js
+++ cypress/e2e/factorial.cy.js
@@ -1,5 +1,37 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../support" />
+
+const fact = function (x) {
+  if (x < 0) {
+    return NaN;
+  }
+  else if (x == 0) {
+    return 1;
+  }
+  else {
+    return x * fact(x - 1);
+  }
+}
+
+describe('Check factorials in range (10,100)', () => {
+  beforeEach(() => {
+    cy.visit('/');
   })
+
+  var i;
+  var j = 10;
+  for (i = 10; i <= 100; i++) {
+    it(`checks factorial of ${i}`, () => {
+      cy.log(j);
+      cy.get('input[id="number"]').should('be.visible').clear().type(j);
+      cy.get('button[id="getFactorial"]').should('be.visible').click();
+      cy.get('p[id="resultDiv"]')
+        .should('be.visible')
+        .and('have.text', `The factorial of ${j} is: ${fact(j)}`);
+      j++;
+    })
+  }
 })
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+// declare namespace Cypress {
+//     interface Chainable<Subject> {
+//       /**
+//        * Example
+//        * @example
+//        * cy.example()
+//        */
+//       example(): Chainable<any>
+//     }
+// }
